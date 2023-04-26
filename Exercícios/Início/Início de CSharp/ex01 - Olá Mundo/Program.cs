8e00cbfc08e64454a05271df590ded7eccb14e0f
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-04-26 15:25:30 -0300

    Upload C#

---
+++ Exercícios/Início/Início de CSharp/ex01 - Olá Mundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, Mundo!");
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
