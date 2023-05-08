73a2ae78b60dead6582a8da1fcd70ed8adb4967a
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-05-08 19:59:46 -0300

    Upload

---
+++ Exercícios/C# - 4 - Exceções/C#_Exceções/csharp_exception/Program.cs
@@ -0,0 +1,45 @@
+﻿using csharp_exception.Titular;
+using csharp_exception.Contas;
+using System;
+using csharp_exception;
+using ByteBank;
+
+LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
+try
+{
+    leitor.LerProximaLinha();
+    leitor.LerProximaLinha();
+    leitor.Fechar();
+}
+catch (IOException)
+{
+    Console.WriteLine("Leitura de arquivo interrompida.");
+}
+finally
+{
+    leitor.Fechar();
+}
+
+/*try
+{
+    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    
+    conta1.Sacar(50);
+    Console.WriteLine(conta1.GetSaldo());
+    conta1.Sacar(150);
+    Console.WriteLine(conta1.GetSaldo());
+    
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Parâmetro com erro: " + ex.ParamName);
+    Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a 0!");
+    Console.WriteLine(ex.StackTrace);
+    Console.WriteLine(ex.Message);
+
+}
+catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Operação negada! Saldo Insuficiente!");
+    Console.WriteLine(ex.Message);
+}*/;
