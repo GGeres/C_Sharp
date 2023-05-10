9cadb11e113b1759303ec07342db7e3b8b3d5270
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-05-10 16:57:33 -0300

    Upload

--- Exercícios/C# - 5 - Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
+++ Exercícios/C# - 5 - Arrays e Coleções/Array_Collections_C-CodigoInicial/bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-TestaArrayInt();
+//TestaArrayInt();
 void TestaArrayInt()
 {
     int[] idades = new int[5];
@@ -22,4 +22,57 @@ void TestaArrayInt()
 
     int media = acumulador / idades.Length;
     Console.WriteLine($"Média de idades = {media}");
+}
+
+//TestaBuscarPalavra();
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+    for ( int i = 0;i < arrayDePalavras.Length; i++)
+    {
+        Console.WriteLine($"Digite {i+1}ª Palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Palavra não encontrada");
+            
+        }
+    }
+}
+
+
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.8, 0);
+amostra.SetValue(4.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(3.5, 3);
+amostra.SetValue(15,4);
+
+TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if (array == null || array.Length == 0)
+    {
+        Console.WriteLine("Array para cálculo está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[]) array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio-1])/2;
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
 }
